5fda94018f627d9aef7014723783815a127480d1
Vee Uye <vee.uye@gmail.com>
2022-03-13 21:10:18 +0000

    Add starting port to new Ship

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,61 +2,60 @@
 const Ship = require("../src/ship");
 const Port = require("../src/port");
 const Itinerary = require("../src/itinerary");
-let ship;
 
-describe("Class Ship", () => {
-  beforeEach(() => {
-    const ceresStation = new Port("Ceres Station");
-    const medinaStation = new Port("Medina Station");
-    const itinerary = new Itinerary([ceresStation, medinaStation]);
-    const ship = new Ship(itinerary);
-  });
-  describe("Ship constructor", () => {
-    it("instantiates a ship", () => {
-      const ceresStation = new Port("Ceres Station");
-      const medinaStation = new Port("Medina Station");
-      const itinerary = new Itinerary([ceresStation, medinaStation]);
-      const ship = new Ship(itinerary);
+describe("Ship constructor", () => {
+  const ceresStation = new Port("Ceres Station");
+  const medinaStation = new Port("Medina Station");
+  const itinerary = new Itinerary([ceresStation, medinaStation]);
+  const ship = new Ship(itinerary);
 
-      // expect(new Ship()).toBeInstanceOf(Object);
-      expect(ship.itinerary.ports).toBeInstanceOf(Array);
-      expect(ship.currentPort).toEqual(ceresStation);
-      expect(ship).toHaveProperty("currentPort");
-      expect(ship.previousPort).toBeNull();
-    });
+  it("instantiates a ship", () => {
+    expect(ship.itinerary.ports).toBeInstanceOf(Array);
+    expect(ship.currentPort).toEqual(ceresStation);
+    expect(ship).toHaveProperty("currentPort");
+    expect(ship.previousPort).toBeNull();
   });
 
-  describe("sailing", () => {
+  it("has a starting port", () => {
     const ceresStation = new Port("Ceres Station");
     const medinaStation = new Port("Medina Station");
     const itinerary = new Itinerary([ceresStation, medinaStation]);
     const ship = new Ship(itinerary);
 
-    it("sets ship sailing", () => {
-      expect(ship.sailShip).toBeInstanceOf(Function);
+    expect(ship.currentPort).toBe(ceresStation);
+  });
+});
 
-      ship.sailShip();
-      expect(ship.previousPort).toEqual(ship.currentPort);
-    });
+describe("sailing", () => {
+  const ceresStation = new Port("Ceres Station");
+  const medinaStation = new Port("Medina Station");
+  const itinerary = new Itinerary([ceresStation, medinaStation]);
+  const ship = new Ship(itinerary);
+
+  it("sets ship sailing", () => {
+    expect(ship.sailShip).toBeInstanceOf(Function);
+
+    ship.sailShip();
+    expect(ship.previousPort).toEqual(ceresStation);
+  });
+});
+
+describe("docking", () => {
+  const ceresStation = new Port("Ceres Station");
+  const medinaStation = new Port("Medina Station");
+  const itinerary = new Itinerary([ceresStation, medinaStation]);
+  const ship = new Ship(itinerary);
+
+  it("can dock at a different port", () => {
+    expect(ship.dockAtPort).toBeInstanceOf(Function);
+    ship.sailShip();
+    ship.dockAtPort();
+    expect(ship.currentPort).toBe(medinaStation);
   });
 
-  describe("docking", () => {
-    const ceresStation = new Port("Ceres Station");
-    const medinaStation = new Port("Medina Station");
-    const itinerary = new Itinerary([ceresStation, medinaStation]);
-    const ship = new Ship(itinerary);
-
-    it("can dock at a different port", () => {
-      expect(ship.dockAtPort).toBeInstanceOf(Function);
-      ship.dockAtPort();
-      expect(ship.currentPort).toBe(medinaStation);
-      console.log(ship.currentPort);
-    });
-
-    // it("can dock at a different port", () => {
-    //   expect(ship.dockAtPort).toBeInstanceOf(Function);
-    //   ship.dockAtPort(medinaStation);
-    //   expect(ship.currentPort).toBe(medinaStation);
-    // });
-  });
+  // it("can dock at a different port", () => {
+  //   expect(ship.dockAtPort).toBeInstanceOf(Function);
+  //   ship.dockAtPort(medinaStation);
+  //   expect(ship.currentPort).toBe(medinaStation);
+  // });
 });
--- src/ship.js
+++ src/ship.js
@@ -3,15 +3,15 @@ class Ship {
     this.itinerary = itinerary;
     this.currentPort = itinerary.ports[0];
     this.previousPort = null;
-    console.log(this.currentPort);
   }
 
   sailShip() {
     this.previousPort = this.currentPort;
+    this.currentPort = null;
   }
   // previously passed in port into dockAtPort
   dockAtPort() {
-    let nextPortIndex = this.itinerary.ports.indexOf(this.currentPort);
+    let nextPortIndex = this.itinerary.ports.indexOf(this.previousPort);
     if (nextPortIndex >= 0 && nextPortIndex < this.itinerary.ports.length - 1)
       this.currentPort = this.itinerary.ports[nextPortIndex + 1];
   }
