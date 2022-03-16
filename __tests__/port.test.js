6f845615ca7fd82c5ae7d7af348f989228ca946e
Vee Uye <vee.uye@gmail.com>
2022-03-16 13:15:48 +0000

    refactor port test suite to use beforeEach

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -4,12 +4,17 @@ const Ship = require("../src/ship");
 const Port = require("../src/port");
 const Itinerary = require("../src/itinerary");
 
-describe("constructor", () => {
-  const port = new Port("The Belt");
+let tychoStation;
+
+describe("port class", () => {
+  beforeEach(() => {
+    tychoStation = new Port("Tycho Station");
+  });
+  // const port = new Port("The Belt");
   it("instantiates a port", () => {
     expect(new Port()).toBeInstanceOf(Object);
   });
   it("port has a name", () => {
-    expect(port.portName).toBe("The Belt");
+    expect(tychoStation.portName).toBe("Tycho Station");
   });
 });
