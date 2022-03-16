f26ca9af9502d87ed846847589318348115d4aaa
Vee Uye <vee.uye@gmail.com>
2022-03-16 13:09:06 +0000

    refactor ship test suite with beforeEach

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,56 +2,48 @@ const Ship = require("../src/ship");
 const Port = require("../src/port");
 const Itinerary = require("../src/itinerary");
 
-it("can be instatiated", () => {
-  const ceresStation = new Port("Ceres Station");
-  const medinaStation = new Port("Medina Station");
-  const itinerary = new Itinerary([ceresStation, medinaStation]);
-  const ship = new Ship(itinerary);
-  expect(ship.itinerary.ports).toBeInstanceOf(Array);
-  expect(ship.currentPort).toEqual(ceresStation);
-  expect(ship).toHaveProperty("currentPort");
-  expect(ship.previousPort).toBeNull();
-});
+let ceresStation;
+let medinaStation;
+let itinerary;
+let ship;
 
-it("has a starting port", () => {
-  const ceresStation = new Port("Ceres Station");
-  const medinaStation = new Port("Medina Station");
-  const itinerary = new Itinerary([ceresStation, medinaStation]);
-  const ship = new Ship(itinerary);
+describe("ship class", () => {
+  beforeEach(() => {
+    ceresStation = new Port("Ceres Station");
+    medinaStation = new Port("Medina Station");
+    itinerary = new Itinerary([ceresStation, medinaStation]);
+    ship = new Ship(itinerary);
+  });
+  it("can be instatiated", () => {
+    expect(ship.itinerary.ports).toBeInstanceOf(Array);
+    expect(ship.currentPort).toEqual(ceresStation);
+    expect(ship).toHaveProperty("currentPort");
+    expect(ship.previousPort).toBeNull();
+  });
 
-  expect(ship.currentPort).toBe(ceresStation);
-});
+  it("has a starting port", () => {
+    expect(ship.currentPort).toBe(ceresStation);
+  });
 
-it("sets ship sailing", () => {
-  const ceresStation = new Port("Ceres Station");
-  const medinaStation = new Port("Medina Station");
-  const itinerary = new Itinerary([ceresStation, medinaStation]);
-  const ship = new Ship(itinerary);
-  expect(ship.sail).toBeInstanceOf(Function);
+  it("sets ship sailing", () => {
+    expect(ship.sail).toBeInstanceOf(Function);
 
-  ship.sail();
-  expect(ship.previousPort).toEqual(ceresStation);
-});
+    ship.sail();
+    expect(ship.previousPort).toEqual(ceresStation);
+  });
 
-it("can dock at a different port", () => {
-  const ceresStation = new Port("Ceres Station");
-  const medinaStation = new Port("Medina Station");
-  const itinerary = new Itinerary([ceresStation, medinaStation]);
-  const ship = new Ship(itinerary);
-  expect(ship.dock).toBeInstanceOf(Function);
-  ship.sail();
-  ship.dock();
-  expect(ship.currentPort).toBe(medinaStation);
-});
+  it("can dock at a different port", () => {
+    expect(ship.dock).toBeInstanceOf(Function);
+    ship.sail();
+    ship.dock();
+    expect(ship.currentPort).toBe(medinaStation);
+  });
 
-it("it can't sail past the last port in itinerary", () => {
-  const ceresStation = new Port("Ceres Station");
-  const medinaStation = new Port("Medina Station");
-  const itinerary = new Itinerary([ceresStation, medinaStation]);
-  const ship = new Ship(itinerary);
-  ship.sail();
-  ship.dock();
-  expect(() => ship.sail()).toThrowError(
-    "You've sailed off the edge of the world and it's turtles all the way down"
-  );
+  it("it can't sail past the last port in itinerary", () => {
+    ship.sail();
+    ship.dock();
+    expect(() => ship.sail()).toThrowError(
+      "You've sailed off the edge of the world and it's turtles all the way down"
+    );
+  });
 });
