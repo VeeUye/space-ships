6e61f7c27924e0a20642ddf9ede506dbf181c3ed
Vee Uye <vee.uye@gmail.com>
2022-03-16 16:26:16 +0000

    add spacing between tests elements for readability

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -17,35 +17,42 @@ describe("port class", () => {
     rocinante = new Ship(itinerary, "Rocinante");
     scopuli = new Ship(itinerary, "Scopuli");
   });
+
   it("instantiates a port", () => {
     expect(new Port(tychoStation)).toBeInstanceOf(Object);
-    console.log(tychoStation.ships);
   });
+
   it("port has a name", () => {
     expect(tychoStation.portName).toBe("Tycho Station");
   });
+
   it("it can add ships", () => {
     tychoStation.ships = [];
     expect(tychoStation.ships).toBeInstanceOf(Array);
     expect(tychoStation.ships).toEqual([]);
     expect(tychoStation.addShip).toBeInstanceOf(Function);
-    expect(tychoStation.removeShip).toBeInstanceOf(Function);
+
     tychoStation.addShip(rocinante);
     expect(tychoStation.ships).toHaveLength(1);
     expect(tychoStation.ships).toEqual([rocinante.name]);
   });
+
   it("can remove ships", () => {
     tychoStation.ships = [];
+    expect(tychoStation.removeShip).toBeInstanceOf(Function);
+
     tychoStation.addShip(rocinante);
     expect(tychoStation.ships).toHaveLength(1);
     expect(tychoStation.ships).toEqual([rocinante.name]);
+
     tychoStation.removeShip(rocinante);
     expect(tychoStation.ships).toEqual([]);
+
     tychoStation.addShip(rocinante);
     tychoStation.addShip(scopuli);
     expect(tychoStation.ships).toEqual([rocinante.name, scopuli.name]);
+
     tychoStation.removeShip(rocinante);
     expect(tychoStation.ships).toEqual([scopuli.name]);
-    console.log(tychoStation.ships);
   });
 });
