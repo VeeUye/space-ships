b91d2820b0443bedaba2265464b1301c8b30ad7f
Vee Uye <vee.uye@gmail.com>
2022-03-16 13:20:54 +0000

    refactor itinerary test suite to use beforeEach

--- __tests__/itinerary.test.js
+++ __tests__/itinerary.test.js
@@ -3,16 +3,23 @@ const Ship = require("../src/ship");
 const Port = require("../src/port");
 const Itinerary = require("../src/itinerary");
 
+let newTerra;
+let ilus;
+let itinerary;
+
 describe("Itinerary", () => {
-  const newTerra = new Port("New Terra");
-  const Ilus = new Port("Ilus");
-  const itinerary = new Itinerary([newTerra, Ilus]);
+  beforeEach(() => {
+    newTerra = new Port("New Terra");
+    ilus = new Port("Ilus");
+    itinerary = new Itinerary([newTerra, ilus]);
+  });
+
   it("instantiates an itinerary", () => {
     expect(itinerary).toBeInstanceOf(Object);
     expect(itinerary).toHaveProperty("ports");
   });
 
   it("has an array of two ports", () => {
-    expect(itinerary.ports).toEqual([newTerra, Ilus]);
+    expect(itinerary.ports).toEqual([newTerra, ilus]);
   });
 });
