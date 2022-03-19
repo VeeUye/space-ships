3184fca34d0b989b80f93f63f6b6ba89ef7b4963
Vee Uye <vee.uye@gmail.com>
2022-03-19 11:06:59 +0000

    wrap class in IIFE

--- src/port.js
+++ src/port.js
@@ -1,13 +1,23 @@
-class Port {
-  constructor(port) {
-    this.portName = port;
-    this.ships = [];
+(function exportPort() {
+  class Port {
+    constructor(port) {
+      this.portName = port;
+      this.ships = [];
+    }
+    addShip(ship) {
+      this.ships.push(ship);
+    }
+    removeShip(ship) {
+      this.ships = this.ships.filter((dockedShip) => dockedShip !== ship);
+    }
   }
-  addShip(ship) {
-    this.ships.push(ship);
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
   }
-  removeShip(ship) {
-    this.ships = this.ships.filter((el) => el !== ship);
-  }
-}
-module.exports = Port;
+})();
+
+// module.exports = Port;
+
+// new Port()
