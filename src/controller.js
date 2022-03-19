4d936dddfc346ec125ce7fcdaa360cfebde176b0
Vee Uye <vee.uye@gmail.com>
2022-03-19 10:01:57 +0000

    Add alternating background image

--- src/controller.js
+++ src/controller.js
@@ -3,6 +3,13 @@ class Controller {
     this.initialiseSea();
   }
   initialiseSea() {
-    window.setInterval(() => {}, 1000);
+    const backgrounds = ["./images/water0.png", "./images/water1.png"];
+    let backgroundIndex = 0;
+    window.setInterval(() => {
+      document.querySelector("#viewport").style.backgroundImage = `url("${
+        backgrounds[backgroundIndex % backgrounds.length]
+      }")`;
+      backgroundIndex += 1;
+    }, 1000);
   }
 }
