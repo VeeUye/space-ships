fedc35de10f4984ca666bfc9e4f787efe25c7ee4
Vee Uye <vee.uye@gmail.com>
2022-03-19 11:33:12 +0000

    Add IFFE to controller

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,22 @@
-class Controller {
-  constructor() {
-    this.initialiseSea();
+(function exportController() {
+  class Controller {
+    constructor() {
+      this.initialiseSea();
+    }
+    initialiseSea() {
+      const backgrounds = ["./images/water0.png", "./images/water1.png"];
+      let backgroundIndex = 0;
+      window.setInterval(() => {
+        document.querySelector("#viewport").style.backgroundImage = `url("${
+          backgrounds[backgroundIndex % backgrounds.length]
+        }")`;
+        backgroundIndex += 1;
+      }, 1000);
+    }
   }
-  initialiseSea() {
-    const backgrounds = ["./images/water0.png", "./images/water1.png"];
-    let backgroundIndex = 0;
-    window.setInterval(() => {
-      document.querySelector("#viewport").style.backgroundImage = `url("${
-        backgrounds[backgroundIndex % backgrounds.length]
-      }")`;
-      backgroundIndex += 1;
-    }, 1000);
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = Controller;
+  } else {
+    window.Controller = Controller;
   }
-}
+})();
